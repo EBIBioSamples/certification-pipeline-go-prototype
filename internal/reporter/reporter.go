cfd8a176361dcee6bdc5c51ecd968df299508d48
Daniel Vaughan <dvaughan@ebi.ac.uk>
2018-11-09 16:45:03 +0000

    added reporter

---
+++ internal/reporter/reporter.go
@@ -0,0 +1,42 @@
+package reporter
+
+import (
+	"github.com/EBIBioSamples/certification-pipeline/internal/model"
+	"log"
+	"sync"
+)
+
+type Reporter struct {
+	logger            *log.Logger
+	certificateIssued chan model.Certificate
+	sync.RWMutex
+	certMap map[string]model.Certificate
+}
+
+func NewReporter(logger *log.Logger, certificateIssued chan model.Certificate) *Reporter {
+	r := Reporter{
+		logger:            logger,
+		certificateIssued: certificateIssued,
+		certMap:           make(map[string]model.Certificate),
+	}
+	r.handleEvents(certificateIssued)
+	return &r
+}
+
+func (r *Reporter) handleEvents(certificateIssued chan model.Certificate) {
+	go func() {
+		for {
+			select {
+			case cert := <-certificateIssued:
+				r.Lock()
+				r.certMap[cert.Sample.UUID] = cert
+				r.Unlock()
+				r.logger.Printf("recorded %s certificate for %s", cert.Checklist.Name, cert.Sample.UUID)
+			}
+		}
+	}()
+}
+
+func (r *Reporter) SampleInfo(uuid string) model.Certificate {
+	return r.certMap[uuid]
+}
