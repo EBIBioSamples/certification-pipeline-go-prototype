e1408af38b90bac17c4c6b05bbce7125ad2aadce
Daniel Vaughan <dvaughan@ebi.ac.uk>
2018-11-22 16:16:20 +0000

    added biosamples example json

--- example.sh
+++ example.sh
@@ -2,3 +2,4 @@
 make && cat res/json/ncbi-SAMN03894263.json | ./bscurate -config=res/config.json -schema=res/schemas/config-schema.json
 curl https://www.ebi.ac.uk/biosamples/samples/SAMN03894263.json -o in.json && cat in.json | ./bscurate -config=res/config.json -schema=res/schemas/config-schema.json
 curl https://www.ebi.ac.uk/biosamples/samples/SAMEA3774859.json -o in.json && cat in.json | ./bscurate -config=res/config.json -schema=res/schemas/config-schema.json
+curl https://wwwdev.ebi.ac.uk/biosamples/samples/SAMEA4669076.json -o in.json && cat in.json | ./bscurate -config=res/config.json -schema=res/schemas/config-schema.json
